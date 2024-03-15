b0b02f9aa782c703dd66d33117e539829d1edf4a
Picafres4 <al02921139@tecmilenio.mx>
2024-03-15 15:57:31 -0600

    Initial Commit

---
+++ Conedu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conedu
+//
+//  Created by athaína rada on 15/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

8f722c1f5fbb2871b1e9c92ab124a89e05667bd7
PatoGD517 <patoguerra517@gmail.com>
2024-03-16 02:05:19 -0600

    version semi fina

---
+++ Conedu/GuiaPersonalizadaViewController.swift
@@ -0,0 +1,56 @@
+//
+//  GuiaPersonalizadaViewController.swift
+//  Conedu
+//
+//  Created by Alumno on 16/03/24.
+//
+
+import Foundation
+import UIKit
+
+class GuiaPersonalizada: UIViewController {
+    
+    @IBOutlet weak var texField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    
+    
+    @IBAction func generarGuia(_ sender: Any) {
+        let count = countLetterS()
+        
+        let alert = UIAlertController(title: "Sugerencias de estudio:", message: count >= 3 ? "¡Felicidades! Has mencionado la letra 'S' más de 3 veces." : "Te sugerimos estudiar más.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+
+    }
+    
+    func countLetterS() -> Int {
+            var count = 0
+            
+            if let text1 = texField1.text {
+                count += text1.lowercased().filter({ $0 == "s" }).count
+            }
+            if let text2 = textField2.text {
+                count += text2.lowercased().filter({ $0 == "s" }).count
+            }
+            if let text3 = textField3.text {
+                count += text3.lowercased().filter({ $0 == "s" }).count
+            }
+            if let text4 = textField4.text {
+                count += text4.lowercased().filter({ $0 == "s" }).count
+            }
+            
+            return count
+        }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+

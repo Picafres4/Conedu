341a88ad48875d86906fca4eb9375fd1b7b45a87
PatoGD517 <patoguerra517@gmail.com>
2024-03-16 02:17:37 -0600

    Version final

--- Conedu/GuiaPersonalizadaViewController.swift
+++ Conedu/GuiaPersonalizadaViewController.swift
@@ -11,41 +11,40 @@ import UIKit
 class GuiaPersonalizada: UIViewController {
     
     @IBOutlet weak var texField1: UITextField!
+    
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
     @IBOutlet weak var textField4: UITextField!
     
     
     @IBAction func generarGuia(_ sender: Any) {
-        let count = countLetterS()
-        
-        let alert = UIAlertController(title: "Sugerencias de estudio:", message: count >= 3 ? "¡Felicidades! Has mencionado la letra 'S' más de 3 veces." : "Te sugerimos estudiar más.", preferredStyle: .alert)
+        let count = contadorLetras()
+               
+        let alert = UIAlertController(title: "Sugerencias de estudio:", message: count >= 3 ? "¡Felicidades! Sigue asi con tus estudios." : "Te sugerimos estudiar más.", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okAction)
-        present(alert, animated: true, completion: nil)
-
+    present(alert, animated: true, completion: nil)
     }
     
-    func countLetterS() -> Int {
-            var count = 0
-            
-            if let text1 = texField1.text {
-                count += text1.lowercased().filter({ $0 == "s" }).count
-            }
-            if let text2 = textField2.text {
-                count += text2.lowercased().filter({ $0 == "s" }).count
-            }
-            if let text3 = textField3.text {
-                count += text3.lowercased().filter({ $0 == "s" }).count
-            }
-            if let text4 = textField4.text {
-                count += text4.lowercased().filter({ $0 == "s" }).count
-            }
-            
-            return count
+    func contadorLetras() -> Int {
+        var count = 0
+        
+        if let text1 = texField1.text {
+            count += text1.lowercased().filter({ $0 == "s" }).count
         }
-    
-    
+        if let text2 = textField2.text {
+            count += text2.lowercased().filter({ $0 == "s" }).count
+        }
+        if let text3 = textField3.text {
+            count += text3.lowercased().filter({ $0 == "s" }).count
+        }
+        if let text4 = textField4.text {
+            count += text4.lowercased().filter({ $0 == "s" }).count
+        }
+        
+        return count
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

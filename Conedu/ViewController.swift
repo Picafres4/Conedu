a1bf8697abf8633d0cf5e14ee06dc28388ee2c36
PatoGD517 <patoguerra517@gmail.com>
2024-03-15 19:10:27 -0600

    login

--- Conedu/ViewController.swift
+++ Conedu/ViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var usuarioLoginField: UITextField!
+    @IBOutlet weak var passwordLoginField: UITextField!
+    
+   
+    
+    @IBAction func login(_ sender: Any) {
+        if (usuarioLoginField.text! == "Conedu" && passwordLoginField.text! == "pwd")
+        {
+            
+            // else, indicar que no se pudo realizar el login por credenciales incorrectas
+        } else {
+            // Alert dialog for failed login
+            let alert = UIAlertController(title: "Login fallido", message: "Credenciales incorrectas.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

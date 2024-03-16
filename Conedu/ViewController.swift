f51c99b5a197516c1104bbd8234fd02b7e6c7e66
Picafres4 <al02921139@tecmilenio.mx>
2024-03-15 19:31:50 -0600

    gfs

--- Conedu/ViewController.swift
+++ Conedu/ViewController.swift
@@ -9,6 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var usuarioLoginField: UITextField!
+    @IBOutlet weak var passwordLoginField: UITextField!
+    
+   
+    
+    @IBAction func login(_ sender: Any) {
+        if (usuarioLoginField.text! == "Conedu" && passwordLoginField.text! == "pwd")
+        {
+            // Login, navegacion a proxima view.
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "MenuID")
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true, completion: nil)
+            // else, indicar que no se pudo realizar el login por credenciales incorrectas
+            
+        } else {
+            // Alert dialog for failed login
+            let alert = UIAlertController(title: "Login fallido", message: "Credenciales incorrectas.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
